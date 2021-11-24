ad5114b39b356e3e3315a70e4dc11b96cb688c66
Devorah Ortega <devorahb.ortega@gmail.com>
2021-11-24 16:52:38 -0500

    Adds PromptsIndex page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    <h3>Please login or sign up</h3>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to Promptly",
+    };
   },
 };
 </script>
