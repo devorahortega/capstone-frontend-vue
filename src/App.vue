c2d89ab1af3027845d5ac04e50f7d706bb141f72
Devorah Ortega <devorahb.ortega@gmail.com>
2021-12-13 16:02:17 -0500

    Adds more pics to about

--- src/App.vue
+++ src/App.vue
@@ -34,96 +34,11 @@
         <a href="/prompts" class="book-a-table-btn scrollto d-none d-lg-flex">Prompts</a>
       </div>
     </header>
+
     <router-view />
-    <footer id="footer" class="midnight-blue"></footer>
+
     <!-- ======= Footer ======= -->
     <footer id="footer">
-      <div class="footer-top">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-3 col-md-6">
-              <div class="footer-info">
-                <h3>Promptly</h3>
-                <p>
-                  A108 Adam Street
-                  <br />
-                  NY 535022, USA
-                  <br />
-                  <br />
-                  <strong>Phone:</strong>
-                  +1 5589 55488 55
-                  <br />
-                  <strong>Email:</strong>
-                  info@example.com
-                  <br />
-                </p>
-              </div>
-            </div>
-
-            <div class="col-lg-2 col-md-6 footer-links">
-              <h4>Useful Links</h4>
-              <ul>
-                <li>
-                  <i class="bx bx-chevron-right"></i>
-                  <a href="#">Home</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i>
-                  <a href="#">About us</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i>
-                  <a href="#">Services</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i>
-                  <a href="#">Terms of service</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i>
-                  <a href="#">Privacy policy</a>
-                </li>
-              </ul>
-            </div>
-
-            <div class="col-lg-3 col-md-6 footer-links">
-              <h4>Our Services</h4>
-              <ul>
-                <li>
-                  <i class="bx bx-chevron-right"></i>
-                  <a href="#">Web Design</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i>
-                  <a href="#">Web Development</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i>
-                  <a href="#">Product Management</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i>
-                  <a href="#">Marketing</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i>
-                  <a href="#">Graphic Design</a>
-                </li>
-              </ul>
-            </div>
-
-            <div class="col-lg-4 col-md-6 footer-newsletter">
-              <h4>Our Newsletter</h4>
-              <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
-              <form action="" method="post">
-                <input type="email" name="email" />
-                <input type="submit" value="Subscribe" />
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-
       <div class="container">
         <div class="copyright">
           &copy; Copyright
