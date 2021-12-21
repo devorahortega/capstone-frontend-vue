46f0dd08e1c31c49d3e0f46fe63e855a73ee751c
Devorah Ortega <devorahb.ortega@gmail.com>
2021-12-21 13:54:08 -0500

    Adds working search bar

--- src/views/PromptsIndex.vue
+++ src/views/PromptsIndex.vue
@@ -9,61 +9,69 @@
           <h2>Prompts</h2>
           <p>List of Prompts</p>
           <br />
-          <!-- <div>
+          <div>
             <form class="example" action="/prompts">
               <input type="text" v-model="titleFilter" placeholder="Search..." list="titles" />
-              <button type="submit"><i class="fa fa-search"></i></button>
               <datalist id="titles">
                 <option v-for="prompt in prompts" :key="prompt.id">{{ prompt.title }}</option>
               </datalist>
+              <button type="submit"><i class="fa fa-search"></i></button>
             </form>
             <br />
             <br />
-          </div> -->
-          <div class="row" data-aos="fade-up" data-aos-delay="100" v-for="prompt in prompts" v-bind:key="prompt.id">
-            <div class="col-lg-3">
-              <ul class="nav nav-tabs flex-column">
-                <li class="nav-item">
-                  <a class="nav-link" data-bs-toggle="tab" id="prompt.id"></a>
-                  <a class="nav-link" data-bs-toggle="tab" id="prompt.id">
-                    <button v-on:click="showForm(prompt)">Edit</button>
-                    <dialog id="prompt-info">
-                      <form method="dialog">
-                        <h1>Prompt Info</h1>
-                        <p>
-                          <input type="text" placeholder="Title" v-model="currentPrompt.title" />
-                        </p>
+          </div>
+          <div
+            v-for="prompt in orderBy(filterBy(prompts, titleFilter, 'title'), sortAttribute)"
+            v-on:click="currentPrompt = prompt"
+            v-bind:class="{ selected: prompt === currentPrompt }"
+            :key="prompt.id"
+          >
+            <div class="row" data-aos="fade-up" data-aos-delay="100">
+              <div class="col-lg-3">
+                <ul class="nav nav-tabs flex-column">
+                  <li class="nav-item">
+                    <a class="nav-link" data-bs-toggle="tab" id="prompt.id"></a>
+                    <a class="nav-link" data-bs-toggle="tab" id="prompt.id">
+                      <button v-on:click="showForm(prompt)">Edit</button>
+                      <dialog id="prompt-info">
+                        <form method="dialog">
+                          <h1>Prompt Info</h1>
+                          <p>
+                            <input type="text" placeholder="Title" v-model="currentPrompt.title" />
+                          </p>
+                          <br />
+                          <p>
+                            <input type="text" placeholder="Content" v-model="currentPrompt.content" />
+                          </p>
+                          <br />
+                          <button v-on:click="updatePrompt(currentPrompt)">Update</button>
+                          <p></p>
+                          <p />
+                        </form>
+                      </dialog>
+                    </a>
+                    <a class="nav-link" data-bs-toggle="tab" id="prompt.id">
+                      <button v-on:click="favoritePrompt(prompt)">Favorite</button>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-lg-9 mt-4 mt-lg-0">
+                <div class="tab-content">
+                  <div class="tab-pane active show">
+                    <div class="row">
+                      <div class="col-lg-8 details order-2 order-lg-1">
+                        <p>{{ prompt.title }}</p>
                         <br />
-                        <p>
-                          <input type="text" placeholder="Content" v-model="currentPrompt.content" />
-                        </p>
+                        <h3>
+                          {{ prompt.content }}
+                        </h3>
+                      </div>
+                      <div class="col-lg-4 text-center order-1 order-lg-2">
+                        <img src="assets/img/specials-1.png" alt="" class="img-fluid" />
                         <br />
-                        <button v-on:click="updatePrompt(currentPrompt)">Update</button>
-                        <p></p>
-                        <p />
-                      </form>
-                    </dialog>
-                  </a>
-                  <a class="nav-link" data-bs-toggle="tab" id="prompt.id">
-                    <button v-on:click="favoritePrompt(prompt)">Favorite</button>
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div class="col-lg-9 mt-4 mt-lg-0">
-              <div class="tab-content">
-                <div class="tab-pane active show">
-                  <div class="row">
-                    <div class="col-lg-8 details order-2 order-lg-1">
-                      <p>{{ prompt.title }}</p>
-                      <h3>
-                        {{ prompt.content }}
-                      </h3>
-                    </div>
-                    <div class="col-lg-4 text-center order-1 order-lg-2">
-                      <img src="assets/img/specials-1.png" alt="" class="img-fluid" />
-                      <br />
-                      <br />
+                        <br />
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -78,17 +86,18 @@
 
 <script>
 import axios from "axios";
-// import Vue2Filters from "vue2-filters";
+import Vue2Filters from "vue2-filters";
 
 export default {
-  // mixins: [Vue2Filters.mixin],
+  mixins: [Vue2Filters.mixin],
   data: function () {
     return {
       prompts: [],
       currentPrompt: "",
       editPromptParams: "",
       errors: [],
-      // user_id: localStorage.getItem("user_id"),
+      titleFilter: "",
+      sortAttribute: "title",
     };
   },
   created: function () {
@@ -125,6 +134,9 @@ export default {
         .catch((error) => {
           this.status = error.response.status;
         });
+    },
+    setSortAttribute: function (inputAttribute) {
+      this.sortAttribute = inputAttribute;
     },
   },
 };
